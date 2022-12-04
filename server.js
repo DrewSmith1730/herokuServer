6d1ec1958158f03977001bd692c4464a3de2cecc
Drew Smith <drewsmith1730@gmail.com>
2022-12-03 19:57:20 -0500

    changed from url to port in server.js

--- server.js
+++ server.js
@@ -3,16 +3,14 @@
 const express = require('express');
 const socketIO = require('socket.io');
 
-const PORT = process.env.BASE_URL;
+const PORT = process.env.PORT;
 const INDEX = '/index.html';
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-const httpServer = createServer(server)
-
-const io = socketIO(httpServer, {/* options */});
+const io = socketIO(server, {/* options */});
 
 io.on('connection', (socket) => {
   console.log('Client connected');
