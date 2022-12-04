4a3b286ae78ebc67b981638a4fa7ee8bbe905e95
Drew Smith <drewsmith1730@gmail.com>
2022-12-04 13:16:48 -0500

    return server to v4

--- server.js
+++ server.js
@@ -1,20 +1,35 @@
 'use strict';
 
 const express = require('express');
-const socketIO = require('socket.io');
+
+// const socketIO = require('socket.io');
 
 const PORT = process.env.PORT;
 const INDEX = '/index.html';
 
-const server = express()
+const app = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
+var server = require('http').createServer(app);
 
-const io = socketIO(server, {/* options */});
+const io = require('socket.io')(server, {/* options */});
+// const io = socketIO(server, {/* options */});
+console.log("Before Connection");
 
-io.on('connection', (ws) => {
-  console.log('Client connected');
-  io.on('close', () => console.log('Client disconnected'));
+io.sockets.on('connection', function(socket) {
+    connections.push(socket);
+    console.log('Connect: %s sockets are connected', connections.length);
+
+    //Disconnect
+    socket.on('disconnect', function(data){
+        connections.splice(connections.indexOf(socket), 1);
+        console.log('Disconnect: %s sockets are connected', connections.length);
+    });
+    
+    socket.on('NodeJS Server Port', function(data) {
+        console.log(data);
+        io.sockets.emit('iOS Client Port', {msg: 'Hi iOS Client!'}, {msg1: ['Hello', 'World']});
+    });
 });
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
