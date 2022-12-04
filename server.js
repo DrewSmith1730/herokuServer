213dab90206b42d2df11de047c264703962b7ab0
Drew Smith <drewsmith1730@gmail.com>
2022-12-04 14:08:50 -0500

    v6.0.5 server

--- server.js
+++ server.js
@@ -6,14 +6,16 @@ const express = require('express');
 const PORT = process.env.PORT;
 const INDEX = '/index.html';
 
-const server = express()
+const app = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+var server = require('http').createServer(app);
 
 // const io = socketIO(server);
 const io = require('socket.io')(server);
 
-io.sockets.on('connection', function(socket) => {
+io.sockets.on('connection', (socket) => {
   console.log('Client connected');
   io.on('close', () => console.log('Client disconnected'));
 });
